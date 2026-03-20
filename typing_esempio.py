13c53691ada4f4c377215f2b9f0810a5e899736a
PietroLao <pittulao04@gmail.com>
2026-03-20 09:37:53 +0100

    Adds file demonstrating Python type hints

---
+++ typing_esempio.py
@@ -0,0 +1,13 @@
+def compute_sum(
+    x:float,
+    y:float,
+    z:float=11
+    ) -> float:
+    return x + y + z
+
+s:str =compute_sum(1,4)
+print(s)
+
+a:dict[str,int]
+a: list | tuple | set | dict | None
+
