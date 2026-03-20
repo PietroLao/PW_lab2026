596107828d79a25bf8559a8b315e4dde19da0c9e
PietroLao <pittulao04@gmail.com>
2026-03-20 10:44:59 +0100

    Adds initial API endpoints for core features
    Introduces several new API endpoints to provide basic application functionality. These routes enhance user interaction by offering a homepage, user-specific pages, and detailed order information.

--- main.py
+++ main.py
@@ -8,3 +8,22 @@ def hello_word(
     sort: bool=False
     ):
     return {"q":q, "sort":sort}
+
+@app.get("/home")
+def homepage():
+    return "Welcome to the home page!"
+
+@app.get("/{username}")
+def username_webpage(
+    username:str
+    ):
+    return f"This is the webpage of {username}."
+
+@app.get("/{username}/orders/{order_id}")
+def order_webpage(
+    username:str,
+    order_id:int,
+    sort: bool=False
+):
+    return f" Order {order_id} for user {username}. Sorted: {sort}"
+
